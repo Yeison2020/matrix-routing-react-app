14961a4d33e3219b11a23066d031b4fb7130d7f5
Yeison <enmanuelquiterios@gmail.com>
2021-11-14 16:49:21 -0500

    Added new dependecies using ttApi

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,27 @@
 import React, { useEffect, useState, useRef } from "react";
 import "./App.css";
+
+// Allow me to see The map using the Api and use It's custom Css
 import "@tomtom-international/web-sdk-maps/dist/maps.css";
 import * as tt from "@tomtom-international/web-sdk-maps";
+// Allow me to see route and easy close route prompt base on the distance and other Circustances
+import * as ttApi from "@tomtom-international/web-sdk-services";
 
 const App = () => {
   const [longitude, setLongitude] = useState(-0.112869);
   const [latitude, setLatitude] = useState(51.504);
   const mapElement = useRef();
   const [map, setMap] = useState({});
+  const convertToPoint = (longLat)=>{
+    return {
+      point:{
+        latitude: longLat.lat, longitude: longLat.lng}}}
+        
   useEffect(() => {
+    const origin = {
+      lng: longitude,
+      lat:latitude
+    }
     let map = tt.map({
       key: process.env.React_App_Tom_Tom_Api_Key,
       container: mapElement.current,
@@ -18,6 +31,12 @@ const App = () => {
     });
     setMap(map);
     const addMarker = () => {
+      const popOffSet = {
+        bottom: [0, -25],
+      };
+      const popup = new tt.Popup({ offset: popOffSet }).setHTML(
+        "Here You're Yeison"
+      );
       const element = document.createElement("div");
       element.className = "marker";
       const marker = new tt.Marker({
@@ -31,8 +50,21 @@ const App = () => {
         setLongitude(longLat.lng);
         setLatitude(longLat.lat);
       });
+      marker.setPopup(popup).togglePopup();
     };
+
     addMarker();
+    const callParameter = {
+      key: process.env.React_App_Tom_Tom_Api_Key,
+      destination: {
+
+      },
+      origin: 
+    };
+    return new Promise((resolve, reject) => {
+      ttApi.services.matrixRouting(callParameter);
+    });
+
     return () => map.remove();
   }, [latitude, longitude]);
   return (
