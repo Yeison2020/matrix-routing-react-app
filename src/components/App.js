9a62424e8f2ec5467fe546cfff8821281d425e0a
Yeison <enmanuelquiterios@gmail.com>
2021-11-14 15:28:58 -0500

    Adding Tom Tom Api key

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  useEffect(() => {
+    let map = tt.map({
+      key: process.env.React_App_Tom_Tom_Api_Key,
+      container: mapElement,
+    });
+  }, []);
   return (
     <div>
       <h1>Hello Wolrd</h1>
