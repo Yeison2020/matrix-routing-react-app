092b84133b4b577bc731cbba2f4fec5863c7e930
Yeison <enmanuelquiterios@gmail.com>
2021-11-14 16:27:24 -0500

    Added Tomtom Css and Fix Zoom issue

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import "./App.css";
+import "@tomtom-international/web-sdk-maps/dist/maps.css";
 import * as tt from "@tomtom-international/web-sdk-maps";
 
 const App = () => {
@@ -16,33 +17,53 @@ const App = () => {
       zoom: 14,
     });
     setMap(map);
+    const addMarker = () => {
+      const element = document.createElement("div");
+      element.className = "marker";
+      const marker = new tt.Marker({
+        draggable: true,
+        element: element,
+      })
+        .setLngLat([longitude, latitude])
+        .addTo(map);
+      marker.on("dragend", () => {
+        const longLat = marker.getLngLat();
+        setLongitude(longLat.lng);
+        setLatitude(longLat.lat);
+      });
+    };
+    addMarker();
     return () => map.remove();
   }, [latitude, longitude]);
   return (
-    <div className="App">
-      <div ref={mapElement} className="map"></div>
-      <div className="search-bar">
-        <h1>Where to?</h1>
-        <input
-          type="text"
-          id="longitude"
-          className="longitude"
-          placeholder="Put in Longitude"
-          onChange={(e) => {
-            setLongitude(e.target.value);
-          }}
-        />
-        <input
-          type="text"
-          id="latitude"
-          className="latitude"
-          placeholder="Put in Latitude"
-          onChange={(e) => {
-            setLatitude(e.target.value);
-          }}
-        />
-      </div>
-    </div>
+    <React.Fragment>
+      {map && (
+        <div className="app">
+          <div ref={mapElement} className="map"></div>
+          <div className="search-bar">
+            <h1>Where to?</h1>
+            <input
+              type="text"
+              id="longitude"
+              className="longitude"
+              placeholder="Put in Longitude"
+              onChange={(e) => {
+                setLongitude(e.target.value);
+              }}
+            />
+            <input
+              type="text"
+              id="latitude"
+              className="latitude"
+              placeholder="Put in Latitude"
+              onChange={(e) => {
+                setLatitude(e.target.value);
+              }}
+            />
+          </div>
+        </div>
+      )}
+    </React.Fragment>
   );
 };
 
